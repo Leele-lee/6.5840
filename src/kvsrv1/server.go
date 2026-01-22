3ae60c6137f6bde2ec6be795519cd61d7b2da6d6
Leele-lee <1623450919@qq.com>
2026-01-22 16:21:14 +0800

    done with kv partA server.go

--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -18,16 +18,21 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type VersionValue struct {
+	Value string
+	Version string
+}
 
 type KVServer struct {
-	mu sync.Mutex
-
 	// Your definitions here.
+	mu sync.Mutex
+	m map[string]VersionValue
 }
 
 func MakeKVServer() *KVServer {
 	kv := &KVServer{}
 	// Your code here.
+	kv.m := make(map[string]VersionValue)
 	return kv
 }
 
@@ -35,6 +40,15 @@ func MakeKVServer() *KVServer {
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here.
+	key := args.Key
+	verVal, ok := m[key]
+	if !ok {
+		reply.Err = ErrNoKey
+		return
+	}
+	reply.Value = verVal.Value
+	reply.Version = verVal.Version
+	return
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -43,6 +57,21 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here.
+	ver := args.Version
+	key := args.Key
+	val := args.Value
+	verVal, ok = m[key]
+	if !ok && ver != 0 !{
+		reply.Err = ErrNoKey
+		return
+	}
+	// If versions don't match, return ErrVersion.
+	if ver != verVal.Version {
+		reply.Err = ErrVersion
+		return
+	}
+	reply.Err = OK
+	return
 }
 
 // You can ignore Kill() for this lab
